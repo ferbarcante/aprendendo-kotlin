0d67e67af2f7609ae6737e82fa48c3b730f561c8
ferbarcante <barcantef@gmail.com>
2024-09-11 12:07:53 -0300

    first commit

---
+++ lógica/variáveis/exemplo01.kt
@@ -0,0 +1,14 @@
+package variáveis
+
+fun main() {
+    // GLOSSÁRIO (var, val, fun, class)
+    // O kotlin automáticamente diz qual tipo da variável armazenada
+    //val é uma variável imutável
+    //var é uma variável mutável
+    val idade = 31
+
+    println(idade)
+
+    println(idade::class)
+
+}
